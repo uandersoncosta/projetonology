b11453b20b2be3b029950c9e4ad4c5c17f4c90be
TI <uandersonferreiracosta@gmail.com>
2026-06-16 11:14:04 -0300

    feat: adicionando a config do banco no app

--- backend/app.py
+++ backend/app.py
@@ -1,10 +1,13 @@
 from flask import Flask
+from flask_migrate import Migrate
+
+from database import db
 
 app = Flask(__name__)
+app.config.from_object('config.Config')
+db.init_app(app)
+migrate = Migrate(app, db)
 
-@app.route('/hello')
-def hello_world():
-  return 'Hello, World!'
 
 if __name__ == '__main__':
   app.run(debug=True, port=8080, host='0.0.0.0')
