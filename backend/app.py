815ead434e00e897a6e697a03559b69612a037e0
TI <uandersonferreiracosta@gmail.com>
2026-06-16 15:02:48 -0300

    feat: rota no app

--- backend/app.py
+++ backend/app.py
@@ -2,12 +2,14 @@ from flask import Flask
 from flask_migrate import Migrate
 from database.db import db
 from models.consulta import Consulta
+from routes.cashback_routes import cashback_bp
 
 app = Flask(__name__)
 app.config.from_object('config.Config')
 db.init_app(app)
 migrate = Migrate(app, db)
 
+app.register_blueprint(cashback_bp)
 
 if __name__ == '__main__':
   app.run(debug=True, port=8080, host='0.0.0.0')
