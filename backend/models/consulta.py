b9a4ad87aa6622a00450098eab0c467f104aec67
TI <uandersonferreiracosta@gmail.com>
2026-06-16 13:57:02 -0300

    fix: alterando importação e adicionando nullable

--- backend/models/consulta.py
+++ backend/models/consulta.py
@@ -1,17 +1,18 @@
-from sqlalchemy.dialects.mysql import UUID
+import uuid
+from sqlalchemy import Uuid
 from sqlalchemy import func
 from database.db import db
 
 class Consulta(db.Model):
     __tablename__ = "consultas"
 
-    id = db.Column(UUID(as_uuid=True), primary_key=True, default=func.uuid_generate_v4())
+    id = db.Column(Uuid, primary_key=True, default=uuid.uuid4)
     ip = db.Column(db.String(50), nullable=False)
-    tipo_cliente = db.Column(db.String(20))
-    valor_compra = db.Column(db.Float)
-    desconto = db.Column(db.Float)
-    cashback = db.Column(db.Float)
-    
+    tipo_cliente = db.Column(db.String(20), nullable=False)
+    valor_compra = db.Column(db.Float, nullable=False)
+    desconto = db.Column(db.Float, nullable=False)
+    cashback = db.Column(db.Float, nullable=False)
+
     def as_dict(self):
         return {
             "id": str(self.id),
