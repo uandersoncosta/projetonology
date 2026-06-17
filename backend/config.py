ffcc55fdb2d6c4c35a377798a9d3e9b33f3b418b
Uanderson Costa <94417952+uandersoncosta@users.noreply.github.com>
2026-06-16 23:45:47 -0300

    fix: adicionando a porta do banco

--- backend/config.py
+++ backend/config.py
@@ -4,13 +4,20 @@ import os
 load_dotenv()
 
 class Config:
-
     SQLALCHEMY_DATABASE_URI = (
         f"mysql+pymysql://"
         f"{os.getenv('DB_USER')}:"
         f"{os.getenv('DB_PASSWORD')}@"
-        f"{os.getenv('DB_HOST')}/"
+        f"{os.getenv('DB_HOST')}:"
+        f"{os.getenv('DB_PORT')}/"
         f"{os.getenv('DB_NAME')}"
     )
 
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+    SQLALCHEMY_ENGINE_OPTIONS = {
+        "connect_args": {
+            "ssl": {}
+        },
+        "pool_pre_ping": True
+    }
